0684d61e4f5c867df3072cade2fec162e75a10c6
Adrian Mikołajczyk <53557466+adimiko@users.noreply.github.com>
2024-03-02 00:32:32 +0100

    Added CNAME (#6)

--- documentation/src/components/HomepageFeatures/index.js
+++ documentation/src/components/HomepageFeatures/index.js
@@ -14,7 +14,7 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Focus on your App',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
