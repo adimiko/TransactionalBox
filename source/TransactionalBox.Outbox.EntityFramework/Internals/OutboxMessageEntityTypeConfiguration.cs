9a181463253902fc4fe20caa4dcc4e0bb020a77d
adimiko <amikolajczyk.it@gmail.com>
2024-03-02 13:04:24 +0100

    Added outbox model

---
+++ source/TransactionalBox.Outbox.EntityFramework/Internals/OutboxMessageEntityTypeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TransactionalBox.Outbox.EntityFramework.Internals
+{
+    internal sealed class OutboxMessageEntityTypeConfiguration : IEntityTypeConfiguration<OutboxMessage>
+    {
+        public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.OccurredUtc);
+            builder.Property(x => x.LockUtc);
+            builder.Property(x => x.ProcessedUtc).IsConcurrencyToken();
+            builder.Property(x => x.Topic);
+            builder.Property(x => x.Payload);
+        }
+    }
+}
---
+++ source/TransactionalBox.Outbox.EntityFramework/ModelBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransactionalBox.Outbox.EntityFramework.Internals;
+
+namespace TransactionalBox.Outbox.EntityFramework
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void AddOutbox(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new OutboxMessageEntityTypeConfiguration());
+        }
+    }
+}
