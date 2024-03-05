415f1170f4270809228f7a66d2d29ecbb3120c69
adimiko <amikolajczyk.it@gmail.com>
2024-03-05 17:34:21 +0100

    Removed unused classes

--- source/TransactionalBox.Inbox/IInbox.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TransactionalBox.Inbox
-{
-    public interface IInbox<T> where T : InboxMessage, new()
-    {
-        Task Handle(T message, CancellationToken cancellationToken);
-    }
-}
