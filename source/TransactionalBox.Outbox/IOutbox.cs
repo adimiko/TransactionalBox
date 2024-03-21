0d737fc3e328116936e019a028f6bb189d95919f
Adrian Mikołajczyk <53557466+adimiko@users.noreply.github.com>
2024-03-21 20:30:13 +0100

    Added InternalsVisibleTo in Outbox (#83)
    * Added InternalsVisibleTo in Outbox

--- source/TransactionalBox.Outbox/IOutbox.cs
+++ source/TransactionalBox.Outbox/IOutbox.cs
@@ -2,7 +2,7 @@
 {
     public interface IOutbox
     {
-        Task Add<TOutboxMessage>(TOutboxMessage message, Action<OutboxMessageMetadata>? metadataConfiguration = null)
+        Task Add<TOutboxMessage>(TOutboxMessage message, Action<Envelope>? envelopeConfiguration = null)
             where TOutboxMessage : class, IOutboxMessage;
     }
 }
