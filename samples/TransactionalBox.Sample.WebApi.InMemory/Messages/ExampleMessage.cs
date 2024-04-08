cb859fd1b9503ccaa81629c3a6adc4d22f72064e
adimiko <amikolajczyk.it@gmail.com>
2024-04-08 19:00:21 +0200

    Added TransactionalBox.Sample.WebApi.InMemory

---
+++ samples/TransactionalBox.Sample.WebApi.InMemory/Messages/ExampleMessage.cs
@@ -0,0 +1,12 @@
+﻿using TransactionalBox.Inbox;
+using TransactionalBox.Outbox;
+
+namespace TransactionalBox.Sample.WebApi.InMemory.Messages
+{
+    public sealed class ExampleMessage : IOutboxMessage, IInboxMessage
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
