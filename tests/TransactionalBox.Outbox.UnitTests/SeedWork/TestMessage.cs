5672fda57a5f74f9b2fff49bd2152232f4f27259
adimiko <amikolajczyk.it@gmail.com>
2024-05-03 10:03:32 +0200

    Refactor

--- tests/TransactionalBox.Outbox.UnitTests/SeedWork/TestMessage.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TransactionalBox.Outbox.UnitTests.SeedWork
-{
-    public sealed class TestMessage : IOutboxMessage
-    {
-        public string Name { get; init; } = "Name";
-    }
-}
---
+++ tests/TransactionalBox.UnitTests/TopicFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using TransactionalBox.Internals;
+using Xunit;
+
+namespace TransactionalBox.UnitTests
+{
+    public sealed class TopicFactoryTests
+    {
+        private readonly ITopicFactory _topicFactory = new TopicFactory();
+
+        [Fact]
+        public void TopicFactoryTest()
+        {
+            // Arrange
+            const string serviceName = "ModuleName";
+            const char separator = '.';
+            var messageName = "TestMessage";
+
+            // Act
+            var actualTopic = _topicFactory.Create(serviceName, messageName);
+
+            // Assert
+            var expectedTopic = serviceName + separator + messageName;
+
+            Assert.Equal(expectedTopic, actualTopic);
+        }
+    }
+}
