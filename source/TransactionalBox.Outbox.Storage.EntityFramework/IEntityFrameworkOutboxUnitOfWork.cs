972821d82c086b956c8ce7ec8a91f81927d95878
adimiko <amikolajczyk.it@gmail.com>
2024-05-08 20:49:43 +0200

    Refactored IEntityFrameworkOutboxUnitOfWork

--- source/TransactionalBox.Outbox.Storage.EntityFramework/IEntityFrameworkOutboxUnitOfWork.cs
+++ source/TransactionalBox.Outbox.Storage.EntityFramework/IEntityFrameworkOutboxUnitOfWork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TransactionalBox.Outbox.Storage.EntityFramework
+﻿namespace TransactionalBox.Outbox.Storage.EntityFramework
 {
     public interface IEntityFrameworkOutboxUnitOfWork
     {
