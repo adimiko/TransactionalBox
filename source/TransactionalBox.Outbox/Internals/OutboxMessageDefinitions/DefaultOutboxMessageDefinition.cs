d6d5546fb23970c69bf331d4400f442c21ae3a68
adimiko <amikolajczyk.it@gmail.com>
2024-05-20 19:29:12 +0200

    Added OutboxMessageDefinition

---
+++ source/TransactionalBox.Outbox/Internals/OutboxMessageDefinitions/DefaultOutboxMessageDefinition.cs
@@ -0,0 +1,7 @@
+﻿namespace TransactionalBox.Outbox.Internals.OutboxMessageDefinitions
+{
+    internal sealed class DefaultOutboxMessageDefinition : IOutboxMessageDefinition
+    {
+        public string? Receiver { get; private set; } = null;
+    }
+}
