c700ea5832245217cbca7c053daebc58d8e265d6
Adrian Mikołajczyk <53557466+adimiko@users.noreply.github.com>
2024-05-23 21:24:21 +0200

    Updated landing page (docs) (#224)
    * Added medium-logo
    
    * Updated colors

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -6,12 +6,15 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
+import logo from '@site/static/img/medium-logo.png';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      
       <div className="container">
+      <img src={logo} className="App-logo" alt="logo" />
         <Heading as="h1" className="hero__title">
           {siteConfig.title}
         </Heading>
@@ -22,6 +25,7 @@ function HomepageHeader() {
             to="/docs/introduction">
             Documentation
           </Link>
+          
         </div>
       </div>
     </header>
