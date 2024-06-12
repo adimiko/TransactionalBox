3403615f3d8a4f5af5974bc35ce935e4543ca7c9
Adrian Mikołajczyk <53557466+adimiko@users.noreply.github.com>
2024-06-12 20:40:30 +0200

    Removed old sample (#285)

--- samples/TransactionalBox.Sample.OutboxWithWorker/ExampleMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TransactionalBox.Sample.OutboxWithWorker
-{
-    public sealed class ExampleMessage : OutboxMessage
-    {
-        public string Name { get; set; } = "Adrian";
-
-        public int Age { get; set; } = 25;
-    }
-}
--- samples/TransactionalBox.Sample.OutboxWithWorker/PublishableMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TransactionalBox.Sample.OutboxWithWorker
-{
-    public class PublishableMessage : OutboxMessage
-    {
-        public string Name { get; set; } = "Adrian";
-
-        public int Age { get; set; } = 25;
-    }
-}
